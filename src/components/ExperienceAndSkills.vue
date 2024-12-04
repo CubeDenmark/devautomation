93e51ab7b9301271e10f86e22722524c4a6744dd
CubeDenmark <cubdesdenmark06242gmail.com>
2024-12-04 17:12:33 +0800

    Contain the portfolio app

---
+++ src/components/ExperienceAndSkills.vue
@@ -0,0 +1,13 @@
+<template>
+    <section class="text-white mt-20" id="services">
+        <div class="px-4 xl:pl-16">
+            <h2 class="text-4xl font-bold text-white mb-4">My Services</h2>
+        </div>
+        <div class="py-8 px-4 sm:py-16 grid grid-cols-1 gap-6 pt-10 sm:grid-cols-2 md:gap-10 md:pt-12 lg:grid-cols-3">
+
+        </div>
+    </section>
+</template>
+<script>
+
+</script>
